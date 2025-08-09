7cf5f4163a1cf8ac8ba8fbabc8bf187aec78f2c0
vasilii <v.verkin@gmail.com>
2025-08-09 12:29:38 +0300

    реализует предпросмотр загруженной фотографии

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getData } from './api.js';
 import './upload-form.js';
+import './preview.js';
 import { renderPictures } from './pictures.js';
 import { setOnFormSubmit } from './upload-form.js';
 import { closeModal } from './modal-control.js';
